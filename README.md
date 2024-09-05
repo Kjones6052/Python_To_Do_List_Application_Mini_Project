7d1d8d2ade266e22ae8528c374feea849e4d9e08
Kjones6052 <letkylehelpyou@gmail.com>
2024-09-05 10:14:25 -0700

    Second Commit, Project Complete

--- README.md
+++ README.md
@@ -3,8 +3,21 @@ Hello, and thank you for using my To-Do-List Application.
 Below you will find an overview of the application, followed by some instructions on how to use this application.
 
 OVERVIEW:
-write overview here.
+With this application, you can create a To-Do List for your self. You can add new tasks to the To-Do list, and you can mark those tasks as 'Complete' once the task has been completed. You can delete a task that you no longer need to do, and at any time you can view the To-Do list to look at all the tasks on the list. When starting the application, you will recieve a welcome message followed by the menu of options to choose from. You can use this application continuously until you choose to 'quit' the application. Enjoy!
 
 INSTRUCTIONS:
 Follow these simple steps to use the application.
-1.
+
+1. Upon starting the application, you will receive a welcome message followed by the menu of options you can choose from. Please type the option you wish to use specifically as it is written on the menu.
+
+2. To addd a task to the To-Do List, type "add a task" when the application asks what you would like to do. It will then ask you what task you would like to add to the list, you can enter the task using your own words but please remember how you worded this task as it will be important later. Once you have entered the task to add to the To-Do List, the application will let you know that this task was added to the list. Lastly, the application will ask you if you would like to continue using the application, type "yes" to continue or "no" to quit.
+
+3. To mark a task as complete, type "mark a task as complete" when the application asks what you would like to do. It will then ask you what task you would like to mark as complete. Type the name of the task you want to complete exactly as you worded it when you added the task to the list. Once you have entered the task to complete, you will receive a message telling you the task is now complete by displaying the task name with the updated task status "Complete". Lastly, the application will ask you if you would like to continue using the application, type "yes" to continue or "no" to quit.
+
+4. To delete a task from the To-Do List, type "delete a task" when the application asks what you would like to do. It will then ask you what task you would like to remove from the To-Do List. Type in the name of the task you want to delete exactly as you worded it when you added the task to the list. Once you have entered the task to delete, the application will send you a message to tell you the task has been removed from the To-Do List. Lastly, the application will ask you if you would like to continue using the application, type "yes" to continue or "no" to quit.
+
+5. You can view the menu of options at any time while using the app. To do this, type "menu" when the application asks what you would like to do. This will show you the menu of options and then ask you if you would like to continue using the application, you can type "yes" to continue or "no" to quit. 
+
+6. To view the To-Do List, type "view tasks" when the application asks what you would like to do. The application will then list out the items on your To-Do List along with their task status of "Complete" or "Incomplete". Lastly, the application will ask you if you would like to continue using the application, type "yes" to continue or "no" to quit.
+
+7. At any point that you wish to quit using the application you can simply type "no" when the application asks if you would like to continue. Once you enter "no", the application will stop running.
--- To_Do_List_Application.py
+++ To_Do_List_Application.py
@@ -1,80 +1,79 @@
 intro = "Welcome to the To-Do List App! Type 'menu' for a list of of options."
-prompt = input("What would you like to do?: ").lower()
+menu = "Menu:\n 1. Add a Task\n 2. View Tasks\n 3. Mark a Task as Complete\n 4. Delete a Task\n 5. Quit Application"
 to_do_list = []
 
 class TaskNotFound(Exception):
     def __init__(self, message):
         self.message = message
 
+def start_app():
+    print(intro)
+    print(menu)
+
 def display_menu():
-    print("Menu:\n 1. Add a Task\n 2. View Tasks\n 3. Mark a Task as Complete\n 4. Delete a Task\n 5. Quit Application")
-    prompt = input("What would you like to do?: ").lower()
+    print(menu)
 
 def add_task():
-    try:
-        new_task = input("What task would you like to add to your To-Do List?: ").capitalize
-        task_status = "Incomplete"
-        if new_task.isalpha():
-            task_item = (f"{new_task}: {task_status}")
-        else:
-            raise ValueError
-    except ValueError:
-        print("The information you entered is not a task. Please enter a task using words.")
-    else:
-        to_do_list.append(task_item)
+    new_task = input("What task would you like to add to your To-Do List?: ")
+    task_status = "Status: Incomplete"
+    task_item = (f"{new_task}: {task_status}")
+    to_do_list.append(task_item)
+    print(f"{task_item} has been added to your To-Do List.")
 
 def view_tasks():
     print(f"To-Do List: {to_do_list}")
 
 def complete_task():
     try:
-        update_task_status = ((input("What task would you like to mark complete?: ") + ": Incomplete")).capitalize
+        check_task_status = (input("What task would you like to mark complete?: "))
+        update_task_status = (f"{check_task_status}: Status: Incomplete")
         for task in to_do_list:
-            if update_task_status == task:
-                task_status_update = "Complete"
-                task_item_update = (f"{update_task_status}: {task_status_update}")
-                to_do_list.replace(task, task_item_update)
-            else:
+            if task == update_task_status:
+                to_do_list.remove(task)
+                task_status_update = "Status: Complete"
+                task_item_update = (f"{check_task_status}: {task_status_update}")
+                to_do_list.append(task_item_update)
+                print(task_item_update)
+            elif task != update_task_status:
                 raise TaskNotFound("The task you have entered is not on the To-Do List. Please try again.")
     except TaskNotFound as e:
         print(f"Error: {e.message}")
 
 def delete_task():
     try:
-        delete_task = (input("What task would you like to delete?: ")).capitalize
+        delete_task = (input("What task would you like to delete?: "))
+        incomplete_task = (f"{delete_task}: Status: Incomplete")
+        complete_task = (f"{delete_task}: Status: Complete")
         for task in to_do_list:
-            completed_task = f"{task} + Complete"
-            incomplete_task = f"{task} + Incomplete"
-            if delete_task == completed_task:
+            if task == complete_task:
                 to_do_list.remove(task)
-            elif delete_task == incomplete_task:
+                print(f"{delete_task} has been removed from the To-Do List.")
+            elif task == incomplete_task:
                 to_do_list.remove(task)
+                print(f"{delete_task} has been removed from the To-Do List.")
             else:
                 raise TaskNotFound("The task you have entered is not on the To-Do List. Please try again.")
     except TaskNotFound as e:
         print(f"Error: {e.message}")
 
-def quit_app():
-    while True:
-        continue_input = input("Would you like to continue using this application? (yes/no): ")
-        if continue_input == "yes":
-            prompt = input("What would you like to do?: ").lower()
-        elif continue_input == "no":
-            break
-        elif prompt == "quit":
-            break
-        else:
-            prompt = input("I'm sorry, an unknown error occurred. What would you like to do?: ").lower()
-
-if prompt == "menu":
-    display_menu()
-elif prompt == "add a task":
-    add_task()
-elif prompt == "view tasks":
-    view_tasks()
-elif prompt == "mark a task as complete":
-    complete_task()
-elif prompt == "delete a task":
-    delete_task()
-elif prompt == "quit application":
-    quit_app()
+start_app()
+while True:
+    prompt = input("What would you like to do?: ").lower()
+    if prompt == "menu":
+        display_menu()
+    elif prompt == "add a task":
+        add_task()
+    elif prompt == "view tasks":
+        view_tasks()
+    elif prompt == "mark a task as complete":
+        complete_task()
+    elif prompt == "delete a task":
+        delete_task()
+    
+    continue_input = input("Would you like to continue using this application? (yes/no): ")
+    if continue_input == "no":
+        break
+    elif continue_input == "yes":
+        continue
+    else:
+        continue_input = input("Error: I did not understand your response. Would you like to continue using this application> (yes/no): ")
